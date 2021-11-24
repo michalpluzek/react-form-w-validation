b70e670c399fba947d54022b853da2a63cb30a1c
Michał Płużek <michalpluzek@gmail.com>
2021-11-24 14:04:29 +0100

    Add form template and state variables

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    checkbox: false,
+  };
+
+  handleChange = (e) => {
+    const username = e.traget.value;
+
+    this.setState({
+      username,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  render() {
+    const { username, email, password, checkbox } = this.state;
+
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="user">
+            Twoje imię:{" "}
+            <input
+              type="text"
+              id="user"
+              name="username"
+              value={username}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="email">
+            E-mail:{" "}
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="password">
+            Hasło:{" "}
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="checkbox">
+            Twoje imię:{" "}
+            <input
+              type="checkbox"
+              id="checkbox"
+              name="checkbox"
+              value={checkbox}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <button>Wyślij formularz</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
