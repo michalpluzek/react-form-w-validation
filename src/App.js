d52f52b123db101228784d97ee5606781c872b6f
Michał Płużek <michalpluzek@gmail.com>
2021-12-01 15:42:04 +0100

    Refactoring to hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-class App extends React.Component {
-  state = {
-    username: "",
-    email: "",
-    password: "",
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkbox, setCheckbox] = useState(false);
+  const [sendForm, setSendForm] = useState(false);
+  const [errors, setErrors] = useState({
+    username: false,
+    email: false,
+    password: false,
     checkbox: false,
-    sendForm: false,
-    errors: {
-      username: false,
-      email: false,
-      password: false,
-      checkbox: false,
-    },
-  };
+  });
 
-  message = {
+  const message = {
     username_incorrect:
       "Nazwa musi być dłuższa niż 3 znaki i nie może zawierać spacji.",
     email_incorrect: "Niewłaściwy adres email.",
@@ -26,53 +24,66 @@ class App extends React.Component {
     form_send: "Formularz został wysłany.",
   };
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const name = e.target.name;
     const type = e.target.type;
     const value = type === "checkbox" ? e.target.checked : e.target.value;
 
-    this.setState({
-      [name]: value,
-    });
+    switch (name) {
+      case "username":
+        setUsername(value);
+        break;
+
+      case "email":
+        setEmail(value);
+        break;
+
+      case "password":
+        setPassword(value);
+        break;
+
+      case "checkbox":
+        setCheckbox(value);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    const validation = this.formValidation();
+    const validation = formValidation();
 
     if (validation.correct) {
-      this.setState({
-        username: "",
-        email: "",
-        password: "",
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setCheckbox(false);
+      setSendForm(true);
+      setErrors({
+        username: false,
+        email: false,
+        password: false,
         checkbox: false,
-        sendForm: true,
-        errors: {
-          username: false,
-          email: false,
-          password: false,
-          checkbox: false,
-        },
       });
     } else {
-      this.setState({
-        sendForm: false,
-        errors: {
-          username: !validation.username,
-          email: !validation.email,
-          password: !validation.password,
-          checkbox: !validation.checkbox,
-        },
+      setSendForm(false);
+      setErrors({
+        username: !validation._username,
+        email: !validation._email,
+        password: !validation._password,
+        checkbox: !validation._checkbox,
       });
     }
   };
 
-  formValidation = () => {
-    let username = false;
-    let email = false;
-    let password = false;
-    let checkbox = false;
+  const formValidation = () => {
+    let _username = false;
+    let _email = false;
+    let _password = false;
+    let _checkbox = false;
     let correct = false;
 
     // if (
@@ -82,119 +93,105 @@ class App extends React.Component {
     //   username = true;
     // }
     const regUsername = /^\S{3,}$/i;
-    username = regUsername.test(this.state.username);
+    _username = regUsername.test(username);
 
     // if (this.state.email.indexOf("@") !== -1) {
     //   email = true;
     // }
     const regEmail = /\w{3,}@[a-z]{2,}\.[a-z]{2,}/i;
-    email = regEmail.test(this.state.email);
+    _email = regEmail.test(email);
 
     // if (this.state.password.length >= 8) {
     //   password = true;
     // }
     const regPassword = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})/;
-    password = regPassword.test(this.state.password);
+    _password = regPassword.test(password);
 
-    if (this.state.checkbox) {
-      checkbox = true;
+    if (checkbox) {
+      _checkbox = true;
     }
 
-    if (username && email && password && checkbox) {
+    if (_username && _email && _password && _checkbox) {
       correct = true;
     }
     return {
       correct,
-      username,
-      email,
-      password,
-      checkbox,
+      _username,
+      _email,
+      _password,
+      _checkbox,
     };
   };
 
-  componentDidUpdate() {
-    if (this.state.sendForm) {
-      setTimeout(
-        () =>
-          this.setState({
-            sendForm: false,
-          }),
-        3000
-      );
+  useEffect(() => {
+    if (sendForm) {
+      setTimeout(() => setSendForm(false), 3000);
     }
-  }
+  }, [sendForm]);
 
-  render() {
-    const { username, email, password, checkbox, sendForm, errors } =
-      this.state;
-    const {
-      username_incorrect,
-      email_incorrect,
-      password_incorrect,
-      checkbox_incorrect,
-    } = this.message;
+  const {
+    username_incorrect,
+    email_incorrect,
+    password_incorrect,
+    checkbox_incorrect,
+  } = message;
 
-    return (
-      <div className="App">
-        <form onSubmit={this.handleSubmit} noValidate>
-          <label htmlFor="user">
-            Twoje imię:
-            <input
-              type="text"
-              id="user"
-              name="username"
-              value={username}
-              onChange={this.handleChange}
-            />
-            {errors.username && (
-              <span className="red">{username_incorrect}</span>
-            )}
-          </label>
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit} noValidate>
+        <label htmlFor="user">
+          Twoje imię:
+          <input
+            type="text"
+            id="user"
+            name="username"
+            value={username}
+            onChange={handleChange}
+          />
+          {errors.username && <span className="red">{username_incorrect}</span>}
+        </label>
 
-          <label htmlFor="email">
-            Twój email:
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-            {errors.email && <span className="red">{email_incorrect}</span>}
-          </label>
+        <label htmlFor="email">
+          Twój email:
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+          {errors.email && <span className="red">{email_incorrect}</span>}
+        </label>
 
-          <label htmlFor="pass">
-            Twoje hasło:
-            <input
-              type="password"
-              id="pass"
-              name="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-            {errors.password && (
-              <span className="red">{password_incorrect}</span>
-            )}
-          </label>
+        <label htmlFor="pass">
+          Twoje hasło:
+          <input
+            type="password"
+            id="pass"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+          {errors.password && <span className="red">{password_incorrect}</span>}
+        </label>
 
-          <label htmlFor="check">
-            <input
-              type="checkbox"
-              id="check"
-              name="checkbox"
-              value={checkbox}
-              onChange={this.handleChange}
-            />
-            Zapoznałem/am się z regulaminem.
-          </label>
-          {errors.checkbox && <span className="red">{checkbox_incorrect}</span>}
+        <label htmlFor="check">
+          <input
+            type="checkbox"
+            id="check"
+            name="checkbox"
+            checked={checkbox}
+            onChange={handleChange}
+          />
+          Zapoznałem/am się z regulaminem.
+        </label>
+        {errors.checkbox && <span className="red">{checkbox_incorrect}</span>}
 
-          <button>Wyślij</button>
-        </form>
-        {sendForm && <h4>{this.message.form_send}</h4>}
-      </div>
-    );
-  }
-}
+        <button>Wyślij</button>
+      </form>
+      {sendForm && <h4>{message.form_send}</h4>}
+    </div>
+  );
+};
 
 export default App;
